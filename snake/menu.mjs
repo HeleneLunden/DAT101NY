42255ddd8d0ada85eccf7a1d52b74404753c2573
HeleneLunden <helenelu@uia.no>
2025-05-05 13:23:01 +0200

    Co-authored-by: Benedicte123 <Benedicte123@users.noreply.github.com>

---
+++ snake/menu.mjs
@@ -0,0 +1,18 @@
+"use strict";
+import lib2D from "../../common/libs/lib2d_v2.mjs";
+import {} from "../"
+
+
+/* Use this file to create the menu for the snake game. */
+export class TMenu {
+  #spMenuBoard;
+  #spcvs;
+  #spPlay
+  #spPause
+  #spHome
+  #spRestart
+  #spResume
+  constructor (aSpriteCanvas) {
+   
+  }
+}
